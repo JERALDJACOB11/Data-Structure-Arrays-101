8ef52bc3834735cb64603e78fbc0e18857281b8f
JERALDJACOB11 <jeraldjacob8a@gmail.com>
2025-04-01 23:15:19 -0700

    c++

---
+++ Max_consecutive_ones/in_C++.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+int findMaxConsecutiveOnes(vector<int>& nums) {
+    int Max = 0; // This variable keeps track of the current count of consecutive 1s
+    int maxco = 0; // This variable keeps track of the maximum count of consecutive 1s found so far
+    
+    // Loop through each element in the given vector (array) 'nums'
+    for(int i = 0; i < nums.size(); i++){
+        if(nums[i] == 1){ // If the current element is 1
+            Max += 1; // Increase the current consecutive count
+            maxco = max(Max, maxco); // Update the maximum count if the current count is greater
+        }else{
+            Max = 0; // If the current element is not 1, reset the current consecutive count to 0
+        }
+    }
+    
+    return maxco; // Return the maximum count of consecutive 1s found
+}
+};
+
