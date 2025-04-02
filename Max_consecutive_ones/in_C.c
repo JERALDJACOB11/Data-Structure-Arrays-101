e92083bc705a2699eeac8e3de073bee65ed2836b
JERALDJACOB11 <jeraldjacob8a@gmail.com>
2025-04-01 23:37:31 -0700

    c

---
+++ Max_consecutive_ones/in_C.c
@@ -0,0 +1,16 @@
+int findMaxConsecutiveOnes(int* nums, int numsSize) {
+    int max = 0; // This variable keeps track of the current count of consecutive 1s
+    int maxco = 0; // This variable keeps track of the maximum count of consecutive 1s found so far
+    
+    // Loop through each element in the given array 'nums'
+    for(int i = 0; i < numsSize; i++) {
+        if(nums[i] == 1) { // If the current element is 1
+            max = max + 1; // Increase the current consecutive count
+            maxco = fmax(max, maxco); // Update the maximum count if the current count is greater
+        } else {
+            max = 0; // If the current element is not 1, reset the current consecutive count to 0
+        }
+    }
+    
+    return maxco; // Return the maximum count of consecutive 1s found
+}
